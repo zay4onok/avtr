a65a176448af8613d09bf5347301a0be0cc46f07
zay4onok <dmitronus2015@gmail.com>
2024-11-15 16:42:00 +0200

    add adaptive design block

--- js/main.js
+++ js/main.js
@@ -7,5 +7,35 @@ $(function () {
       '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="" />',
     nextArrow:
       '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="" />',
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
